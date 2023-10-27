88a5db589c44b5b478b94db822b2eb3610b2e90d
achibulup <achibulup@gmail.com>
2023-10-27 21:12:39 +0700

    run script

--- multi_stage_approach/run.sh
+++ multi_stage_approach/run.sh
@@ -1,9 +1,12 @@
-# first stage.
-python3 main.py --file_type="Car-COQE" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 
-python3 main.py --file_type="Ele-COQE" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 
-python3 main.py --file_type="Camera-COQE" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 
+python3 main.py --dataset="VLSP2023" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 
+python3 main.py --dataset="VLSP2023" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
 
-# second and thrid stage.
-python3 main.py --file_type="Car-COQE" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
-python3 main.py --file_type="Ele-COQE" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
-python3 main.py --file_type="Camera-COQE" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
+# # first stage.
+# python3 main.py --dataset="Car-COQE" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 
+# python3 main.py --dataset="Ele-COQE" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 
+# python3 main.py --dataset="Camera-COQE" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 
+
+# # second and thrid stage.
+# python3 main.py --dataset="Car-COQE" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
+# python3 main.py --dataset="Ele-COQE" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
+# python3 main.py --dataset="Camera-COQE" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --factor=0.3
